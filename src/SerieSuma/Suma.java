96765da2f3e460832ff208e36fd07f436a4cb26e
Jean-GG <jcgaribayg02@hotmail.com>
2021-10-26 13:45:22 -0500

    Update3

--- src/SerieSuma/Suma.java
+++ src/SerieSuma/Suma.java
@@ -4,7 +4,17 @@ public class Suma {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("Prueba");
+		int numero1 = 0;
+		int numero2 = 1;
+		int suma = 0;
+		int cantidadNumeros = 10;
+		
+		for (int x = 0; x<=cantidadNumeros ; x++ ) {
+			suma = numero1 + numero2;
+			numero1 = numero2;
+			numero2 = suma;
+
+			System.out.println(suma);
 	}
 
 }
